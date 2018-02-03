e084208cb262c50066f525061bbf10f51f2a4436
JachuPL <bonkowskijan@gmail.com>
2018-02-03 21:19:27 +0100

    Security patch that prevents edit/modify/remove other users collections

--- collectionsapp/views.py
+++ collectionsapp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import authenticate, login, logout
-from .models import *
+from django.shortcuts import render, get_object_or_404, redirect
+
 from .forms import *
 
 
@@ -32,7 +32,10 @@ def login_user(request):
 
 
 def register(request):
-	form = UserForm(request.POST or None)
+	if request.user.is_authenticated:
+		return redirect('collections:index')
+
+	form = UserForm(request.POST)
 	if form.is_valid():
 		user = form.save(commit=False)
 		username = form.cleaned_data['username']
@@ -43,8 +46,7 @@ def register(request):
 		if user is not None:
 			if user.is_active:
 				login(request, user)
-				collections = Collection.objects.filter(user=request.user)
-				return render(request, 'collections/index.html', {'collections': collections})
+				return redirect('collections:index')
 	context = {
 		"form": form,
 	}
@@ -52,87 +54,110 @@ def register(request):
 
 
 def index(request):
-    collections = Collection.objects.all()
-    context = {'collections': collections}
-    return render(request, 'collections/index.html', context)
+	collections = Collection.objects.all()
+	context = {'collections': collections}
+	return render(request, 'collections/index.html', context)
 
 
 def detail(request, collection_id):
-    collection = get_object_or_404(Collection, pk=collection_id)
-    collection_items = CollectionValue.objects.filter(collection_id=collection_id)
-    context = {
-        'collection': collection,
-        'collection_items': collection_items
-    }
-    return render(request, 'collections/detail.html', context)
+	collection = get_object_or_404(Collection, pk=collection_id)
+	collection_items = CollectionValue.objects.filter(collection_id=collection_id)
+	context = {
+		'collection': collection,
+		'collection_items': collection_items
+	}
+	return render(request, 'collections/detail.html', context)
 
 
 def create_collection(request):
-    if request.method == "POST":
-        form = CollectionForm(request.POST)
-        if form.is_valid():
-            collection = form.save(commit=False)
-            collection.save()
-            return redirect('collections:detail', collection_id=collection.pk)
-    else:
-        form = CollectionForm()
-    return render(request, 'collections/create_collection.html', {"form": form})
+	if not request.user.is_authenticated:
+		return redirect('collections:login')
+	if request.method == "POST":
+		form = CollectionForm(request.POST)
+		if form.is_valid():
+			collection = form.save(commit=False)
+			collection.user = request.user
+			collection.save()
+			return redirect('collections:detail', collection_id=collection.pk)
+	else:
+		form = CollectionForm()
+	return render(request, 'collections/create_collection.html', {"form": form})
 
 
 def delete_collection(request, collection_id):
-    colletion = Collection.objects.get(pk=collection_id)
-    colletion.delete()
-    return redirect("/")
+	if request.user.is_authenticated:
+		collection = Collection.objects.get(pk=collection_id)
+		if request.user == collection.user:
+			collection.delete()
+	return redirect("/")
 
 
 def edit_collection(request, collection_id):
-    collection = get_object_or_404(Collection, pk=collection_id)
-    if request.method == "POST":
-        form = CollectionForm(request.POST or None, instance=collection)
-        if form.is_valid():
-            collection = form.save(commit=False)
-            collection.save()
-            return redirect('collections:detail', collection_id=collection.pk)
-    else:
-        form = CollectionForm(instance=collection)
-    return render(request, 'collections/edit_collection.html', {"form": form})
+	if not request.user.is_authenticated:
+		return redirect('collections:login')
+
+	collection = get_object_or_404(Collection, pk=collection_id)
+	if not request.user == collection.user:
+		return redirect('collections:index')
+
+	if request.method == "POST":
+		form = CollectionForm(request.POST or None, instance=collection)
+		if form.is_valid():
+			collection = form.save(commit=False)
+			collection.save()
+			return redirect('collections:detail', collection_id=collection.pk)
+	else:
+		form = CollectionForm(instance=collection)
+	return render(request, 'collections/edit_collection.html', {"form": form})
 
 
 def item(request, value_id):
-    collection_value = get_object_or_404(CollectionValue, pk=value_id)
-    context = {'value': collection_value}
-    return render(request, 'collections/item.html', context)
+	collection_value = get_object_or_404(CollectionValue, pk=value_id)
+	context = {'value': collection_value}
+	return render(request, 'collections/item.html', context)
 
 
 def create_item(request, collection_id):
-    if request.method == "POST":
-        collection = get_object_or_404(Collection, pk=collection_id)
-        form = ItemForm(request.POST)
-        if form.is_valid():
-            collection_item = form.save(commit=False)
-            collection_item.collection_id = collection
-            collection_item.save()
-            return redirect('collections:item', value_id=collection_item.id)
-    else:
-        form = ItemForm()
-    return render(request, 'collections/create_item.html', {"form": form})
+	if not request.user.is_authenticated:
+		return redirect('collections:login')
+
+	collection = get_object_or_404(Collection, pk=collection_id)
+	if not request.user == collection.user:
+		return redirect('collections:detail', collection_id=collection_id)
+
+	if request.method == "POST":
+		form = ItemForm(request.POST)
+		if form.is_valid():
+			collection_item = form.save(commit=False)
+			collection_item.collection_id = collection
+			collection_item.save()
+			return redirect('collections:item', value_id=collection_item.id)
+	else:
+		form = ItemForm()
+	return render(request, 'collections/create_item.html', {"form": form})
 
 
 def delete_item(request, value_id):
-    collection_item = CollectionValue.objects.get(pk=value_id)
-    collection_id = collection_item.collection_id.pk
-    collection_item.delete()
-    return redirect("collections:detail", collection_id=collection_id)
+	if request.user.is_authenticated:
+		collection_item = CollectionValue.objects.get(pk=value_id)
+		if request.user == collection_item.collection_id:
+			collection_id = collection_item.collection_id.pk
+			collection_item.delete()
+	return redirect("collections:detail", collection_id=collection_id)
 
 
 def edit_item(request, value_id):
-    collection_item = get_object_or_404(CollectionValue, pk=value_id)
-    if request.method == "POST":
-        form = ItemForm(request.POST or None, instance=collection_item)
-        if form.is_valid():
-            collection_item = form.save(commit=False)
-            collection_item.save()
-            return redirect('collections:item', value_id=collection_item.pk)
-    else:
-        form = ItemForm(instance=collection_item)
-    return render(request, 'collections/edit_item.html', {"form": form})
+	collection_item = get_object_or_404(CollectionValue, pk=value_id)
+
+	if not request.user.is_authenticated or not request.user == collection_item.collection_id.user:
+		return redirect("collections:detail", collection_id=collection_item.collection_id.pk)
+
+	if request.method == "POST":
+		form = ItemForm(request.POST or None, instance=collection_item)
+		if form.is_valid():
+			collection_item = form.save(commit=False)
+			collection_item.save()
+			return redirect('collections:item', value_id=collection_item.pk)
+	else:
+		form = ItemForm(instance=collection_item)
+	return render(request, 'collections/edit_item.html', {"form": form})
